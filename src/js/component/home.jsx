a6c211d6a3f0e8e6ec48fbc9c201c31112640258
dedalovitor <dedalovitor@gmail.com>
2024-04-10 15:42:29 +0000

    changes 2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Light from "./light";
 
+
+/*
+ * En la home definimos los useStates donde vamos a almacenar los diferentes estados: 
+ * - selectedColor: determina la luz que se encuentra seleccionada y setSelectedColor permite definir la nueva luz 
+ * que se va a seleccionar, se inicializa en un string vacio "".
+ * - colors: es un array de colores, donde podemos meter más colores mediate setColors, y que podemos recorrer para hacer el map de las luces
+ * - AutoChangeActive: se inicializa en false y permite setear el estado del botón de cambio de luz. De esta forma activo hace un start del
+ * cambio automático de luz cada 3 segundos.
+ * - intervalId: 
+ */
+
+
 const Home = () => {
     const [selectedColor, setSelectedColor] = useState("");
     const [colors, setColors] = useState(["red", "yellow", "green"]);
@@ -16,21 +28,40 @@ const Home = () => {
             setColors([...colors, "purple"]);
         }
     };
-    
+/**
+ * Esta función se activa al clicar sobre el botón "Add Extra Purple Color 2".
+ * Como lo que queremos es que se cree una vez y se borre la luz purple al clicar y volver a clicar sobre el botón
+ * creamos una variable con la que determinamos si purple ha sido incluida en el array colors
+ * con if condicional determinamos dentro de una nueva variable updatedColors que si ha sido incluida haga un .filter de todos los colores menos el purple
+ * y seteamos el array de colores a updatedColors, si no ha sido incluido, seteamos el array de colores añadiendole purple. 
+ * De esta forma conseguimos que se muestre la nueva luz morada en el semáforo, habiendo sido necesario para ello utilizar un estado para poder setearla 
+ * y renderizar el elemento inmediatamente. 
+ */
+
         const toggleAutoChange = () => {
             if (autoChangeActive) {
                 clearInterval(intervalId);
                 setIntervalId(null);
             } else {
                 const id = setInterval(() => {
-                    setSelectedColor(prevColor => (prevColor === colors.length - 1 ? 0 : prevColor + 1));
+                    setSelectedColor(prevColor => (prevColor === colors.length - 1 ? 0 : prevColor + 1)); // no entiendo por qué se utiliza el prevColor, revisar qué significa esa sintaxis 
                 }, 3000);
                 setIntervalId(id);
             }
             setAutoChangeActive(!autoChangeActive);
         };
+
+        /*
+         * Con esta función utilizamos un condicional para indicar que si nos encontramos con que el estado de 
+         * autoChangeActive es false, limpiamos el intervalId y lo seteamos a null, de manera que paramos de cambiar automáticamente el color del semáforo.
+         * Si autoChangeActive lo ponemos a true se activa la condición else en la que creamos una variable llamada id, en la que seteamos el Intervalo en el
+         * que cambiamos de color y le decimos que si el color es el último del array que nos cambie al primero y que si es cualquier otro que nos cambie al siguiente,
+         * esto lo hace cada 3 segundos. 
+         * La última línea nos permite inicializar y hacer stop del setAutoChangeActive, ya que al invocarlo le decimos que su estado será el contrario del que estaba,
+         * pasa de activo a inactivo y viceversa.
+        */
     
-        useEffect(() => {
+        /*useEffect(() => {
             if (autoChangeActive && !intervalId) {
                 const id = setInterval(() => {
                     setSelectedColor(prevColor => (prevColor === colors.length - 1 ? 0 : prevColor + 1));
@@ -42,7 +73,7 @@ const Home = () => {
                     clearInterval(intervalId);
                 }
             };
-        }, [autoChangeActive, intervalId, colors]);
+        }, [autoChangeActive, intervalId, colors]);*/
 
     const handleChangeColor = () => {
         setSelectedColor(selectedColor === colors.length - 1 ? 0 : selectedColor + 1);
--- src/js/component/light.js
+++ src/js/component/light.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 
+/*
+Hemos creado el componente Light al que le pasamos las props: 
+- selectedColor (índice del color seleccionado: 0 red, 1 yellow, 2 green..)
+- item: es cada uno de los colores dentro del array colors
+- index: es cada uno de los índices del array color
+- setSelectedColor: es la función del useState que nos permite setear el estado del color seleccionado.
+
+De esta forma, mediante un .map del array colors vamos a recorrerlo y renderizar una luz del semáforo por cada color.
+La primera funcionalidad integrada es que si clicamos sobre una luz la seleccionamos, y al seleccionarla hacemos 
+que se ilumine " glow " (definido como clase en css), si no está seleccionada deja de iluminarse.
+La manera de configurar el comportamiento y estética de cada luz es a través del className, donde utilizamos el
+literal string `..` y dentro le pasamos las propiedades  
+*/
+
 const Light = ({selectedColor, item, index, setSelectedColor})=>{
     return (
         <div key={index}
                 onClick={() => setSelectedColor(index)}
-                className={`light ${item} + ${selectedColor === index ? " glow " : " "} `}>
+                className={`light ${item}  ${selectedColor === index ? " glow " : " "} `}>
             </div>
     );
 };
